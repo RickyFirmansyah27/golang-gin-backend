a0793b11cebae3a3a03841bb4da963e0c7468679
RickyFirmansyah27 <ricky_firmansyah@teknokrat.ac.id>
2025-05-05 15:44:44 +0700

    fix route

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"golang-vercel/app/controllers"
 	"golang-vercel/app/helpers"
-	"log"
 
 	"net/http"
 
@@ -13,12 +12,10 @@ import (
 func RootRoute(app *gin.Engine) {
 	app.NoRoute(noAccessRoute)
 
-	// Add logging here for debugging
-	log.Println("Defining routes...")
 	app.GET("/items", controllers.GetAllItems)
 	app.POST("/items", controllers.CreateItem)
 	app.PATCH("/items", controllers.UpdateItem)
-	app.DELETE("/items", controllers.DeleteItem)
+	app.DELETE("/items/:id", controllers.DeleteItem)
 }
 
 func noAccessRoute(c *gin.Context) {
